ea12628f60ad7e6e6015b51b2dc233fbe4c47b2a
Ricky Garcia <rickyg08@msn.com>
2021-01-13 11:41:27 -0700

    Fixed some errors.

--- app/src/main/java/edu/cnm/deepdive/gameoflife/controller/MainActivity.kt
+++ app/src/main/java/edu/cnm/deepdive/gameoflife/controller/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         ViewModelProvider(this).get(MainViewModel::class.java).also {
             viewModel = it
